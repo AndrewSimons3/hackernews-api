e6e8c7772ff83e2339dbcf1e265ef7b525eb62f3
AndrewSimons3 <simdrew07@gmail.com>
2020-03-26 21:47:56 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    
-  );
+class App extends React.Component {
+  state = {
+    stories: []
+  }
+
+
+  componentDidMount() {
+    fetch("http://hn.algolia.com/api/v1/search?tags=story,author_pg").then( (resp) => {
+      return resp.json();
+  }).then( (data) => {
+    this.setState({stories: data.hits})
+  });
+  }
+
+  getArticle(stories) {
+    return `${stories.title} ${stories.author} ${stories.created_at}`
+  }
+
+
+  render() {
+  
+    return(
+
+      <div>
+      <form>
+        <label>
+          Author
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Search" />
+      </form>
+      <form>
+        <label>
+          Date
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Search" />
+      </form>
+      <ul>
+      {this.state.stories.map((story, index) => {
+        return (<li key={index}>
+          <span>{this.getArticle(story)}</span>
+      }
+        </li>)
+      })}
+      </ul>
+      </div>
+     
+    )
+  }
 }
 
 export default App;
