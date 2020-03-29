91f8163f58e1fd9241946ab90153d78ad4e66199
AndrewSimons3 <simdrew07@gmail.com>
2020-03-29 11:29:36 -0500

    started

--- src/App.js
+++ src/App.js
@@ -7,17 +7,29 @@ class App extends React.Component {
   }
 
 
-  componentDidMount() {
+  // componentDidMount() {
+  //   fetch("http://hn.algolia.com/api/v1/search?tags=story,author_pg").then( (resp) => {
+  //     return resp.json();
+  // }).then( (data) => {
+  //   this.setState({stories: data.hits})
+  // });
+  // }
+
+  searchByAuthor = () => {
     fetch("http://hn.algolia.com/api/v1/search?tags=story,author_pg").then( (resp) => {
       return resp.json();
   }).then( (data) => {
     this.setState({stories: data.hits})
   });
-  }
+}
 
-  getArticle(stories) {
-    return `${stories.title} ${stories.author} ${stories.created_at}`
-  }
+  searchByDate = () => {
+    fetch("http://hn.algolia.com/api/v1/search_by_date?tags=story").then( (resp) => {
+      return resp.json();
+  }).then( (data) => {
+    this.setState({stories: data.hits})
+  });
+}
 
 
   render() {
@@ -25,30 +37,23 @@ class App extends React.Component {
     return(
 
       <div>
-      <form>
+      <form onSubmit={this.searchByAuthor}>
         <label>
           Author
           <input type="text" name="name" />
         </label>
-        <input type="submit" value="Search" />
+        <input type="submit" value="Search"></input>
+        {this.state.stories.map(
+        
       </form>
-      <form>
+      <form onSubmit={this.searchByDate}>
         <label>
           Date
           <input type="text" name="name" />
         </label>
-        <input type="submit" value="Search" />
+        <input type="submit" value="Search"></input>
       </form>
-      <ul>
-      {this.state.stories.map((story, index) => {
-        return (<li key={index}>
-          <span>{this.getArticle(story)}</span>
-      }
-        </li>)
-      })}
-      </ul>
       </div>
-     
     )
   }
 }
